3c719a42637dd45973dadbaeac5f95379feebdd5
naturalone <xnorish@gmail.com>
2020-01-08 00:35:35 +0200

    client/redux: added alert reducer, added auth reducer & action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,7 @@
+// alert.js
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
+
+// auth.js
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
